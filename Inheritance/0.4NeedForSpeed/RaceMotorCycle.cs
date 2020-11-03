7e306a012946665f8376d5ca88c5242047ab10e0
Mihail Shkembov <mihailshkembov@gmail.com>
2020-11-03 13:03:17 +0200

    InheritanceExercise

---
+++ Inheritance/0.4NeedForSpeed/RaceMotorCycle.cs
@@ -0,0 +1,10 @@
+﻿namespace NeedForSpeed.Motorcycle
+{
+    public class RaceMotorcycle : Motorcycle
+    {
+        public RaceMotorcycle(int horsePower, double fuel) : base(horsePower, fuel)
+        {
+        }
+        public override double DefaultFuelConsumption => 8;
+    }
+}
